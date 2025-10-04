f0ceebc615d7635465fa54bc95fd723f32d3bcf5
Arshad Ullah <mdarshadullah835@gmail.com>
2025-10-04 20:11:08 +0600

    loop practice

--- loop.js
+++ loop.js
@@ -4,3 +4,78 @@ const numbers = [10, 20, 5, 21, 33];
 // for (const name of names) {
 //   console.log(name);
 // }
+
+// const names = ["korim", "rohim", "solim", "rohomot", "shoikot"];
+// for (const name of names) {
+//   console.log(name);
+// }
+
+// let count = 1;
+
+// while (count < 9) {
+//   console.log(count);
+//   count++;
+// }
+
+// let count = 1;
+// let sum = 0;
+// while (count <= 1000) {
+//   sum = count + sum;
+
+//   count++;
+// }
+// console.log(sum);
+
+// let count = 1;
+// // let sum = 0;
+
+// while (count <= 100) {
+//   if (count % 2 !== 0) {
+//     console.log(count);
+//   }
+//   count++;
+// }
+// let num = 1;
+// let text = "i love bangladesh";
+
+// while (num <= 100) {
+//   console.log(num, text);
+
+//   num++;
+// }
+
+// let sum = 0;
+// for (let i = 1; i <= 10; i++) {
+//   sum = sum + i;
+// }
+// console.log(sum);
+
+// let num = 0;
+// let sum = 0;
+// while (num <= 10) {
+//   sum = sum + num;
+//   num++;
+// }
+
+// console.log(sum);
+
+// let num = 61;
+// while (num <= 99) {
+//   if (num % 2 !== 0) {
+//     console.log(num);
+//   }
+//   num++;
+// }
+
+// for (let evenNumber = 78; evenNumber <= 88; evenNumber++) {
+//   if (evenNumber % 2 === 0) {
+//     console.log(evenNumber);
+//   }
+// }
+// let num = 78;
+// while (num < 88) {
+//   if (num % 2 === 0) {
+//     console.log(num);
+//   }
+//   num++;
+// }
