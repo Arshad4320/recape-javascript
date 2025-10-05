4a73962a5477a0ec4ca5f39658477eaaf7fe08d5
Arshad Ullah <mdarshadullah835@gmail.com>
2025-10-05 23:34:29 +0600

    another string problem solve

--- string.js
+++ string.js
@@ -7,17 +7,45 @@
 //   }
 // }
 // console.log(count);
-const str = " Check whether a string contains all the vowels a, e, i, o, u";
+// const str = " Check whether a string contains all the vowels a, e, i, o, u";
 
-let count = 0;
-for (const string of str) {
-  if (
-    string === "a" ||
-    string === "e" ||
-    string === "i" ||
-    string === "o" ||
-    string === "u"
-  ) {
-    console.log(string);
-  }
-}
+// let count = 0;
+// for (const string of str) {
+//   if (
+//     string === "a" ||
+//     string === "e" ||
+//     string === "i" ||
+//     string === "o" ||
+//     string === "u"
+//   ) {
+//     console.log(string);
+//   }
+// }
+
+// const cher =
+//   "If a given string has either x, replace x by y. if the given string has X, replace it by Y.";
+// let newStr = "";
+// for (const ch of cher) {
+//   if (ch === "x") {
+//     newStr += "y";
+//   } else {
+//     newStr += ch;
+//   }
+// }
+// console.log(newStr);
+// const cher =
+//   "If a given string has either x, replace x by y. if the given string has X, replace it by Y.";
+
+// let newStr = "";
+
+// for (const ch of cher) {
+//   if (ch === "x") {
+//     newStr += "y";
+//   } else if (ch === "X") {
+//     newStr += "Y";
+//   } else {
+//     newStr += ch;
+//   }
+// }
+
+// console.log(newStr);
