492156878ed7007bcf9cc0d1f81c2d16e65d1a60
Arshad Ullah <mdarshadullah835@gmail.com>
2025-10-05 23:09:04 +0600

    string problem practice done

---
+++ string.js
@@ -0,0 +1,9 @@
+// const stringCher = "Count how many times a string has the letter a";
+
+// let count = 0;
+// for (const str of stringCher) {
+//   if (str === "e") {
+//     count++;
+//   }
+// }
+// console.log(count);
