f485a33e5b550eb05366ecd2aca90ea4de033518
Arshad Ullah <mdarshadullah835@gmail.com>
2025-10-05 23:20:35 +0600

    another provlem solved

--- string.js
+++ string.js
@@ -7,3 +7,17 @@
 //   }
 // }
 // console.log(count);
+const str = " Check whether a string contains all the vowels a, e, i, o, u";
+
+let count = 0;
+for (const string of str) {
+  if (
+    string === "a" ||
+    string === "e" ||
+    string === "i" ||
+    string === "o" ||
+    string === "u"
+  ) {
+    console.log(string);
+  }
+}
