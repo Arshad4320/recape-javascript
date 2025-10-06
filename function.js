fdb8dc65ff1b5bdcc385fe10f6ea5a3b75ea2c7a
Arshad Ullah <mdarshadullah835@gmail.com>
2025-10-06 11:45:06 +0600

    another function practice done

--- function.js
+++ function.js
@@ -4,13 +4,24 @@
 // const bill = add(10, 20);
 // console.log(bill);
 
-function stringSize(str) {
-  if (str.length % 2 === 0) {
-    console.log("even size str");
-  } else {
-    console.log("odd size str");
+// function stringSize(str) {
+//   if (str.length % 2 === 0) {
+//     console.log("even size str");
+//   } else {
+//     console.log("odd size str");
+//   }
+//   console.log(str, str.length);
+// }
+// stringSize("Bangladesh");
+// stringSize("India");
+let a = 0;
+function add(number) {
+  for (const num of number) {
+    a = a + num;
   }
-  console.log(str, str.length);
+  return a;
 }
-stringSize("Bangladesh");
-stringSize("India");
+
+const addedNumber = [10, 20, 30, 40, 50];
+const total = add(addedNumber);
+console.log(total);
