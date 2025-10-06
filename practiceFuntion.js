f03f5dacc5a9691c6450754628e59ec89e4b6a8e
Arshad Ullah <mdarshadullah835@gmail.com>
2025-10-06 12:27:06 +0600

    another function done

--- practiceFuntion.js
+++ practiceFuntion.js
@@ -5,3 +5,24 @@
 // }
 // const result = multiplyNumber(2, 3, 4, 5);
 // console.log(result);
+// Take a number if the number is odd multiply it by 2 and return the result. If the number is even divide it by two and return the result.
+
+// function numberCheck(number) {
+//   if (number % 2 === 1) {
+//     return number * 2;
+//   } else {
+//     return number / 2;
+//   }
+// }
+// console.log(numberCheck(5));
+// console.log(numberCheck(6));
+// Write a function called make_avg() which will take an array of integers and the size of that array and return the average of those values.
+// let a = 0;
+// function avg(number) {
+//   for (const num of number) {
+//     a = a + num / number.length;
+//   }
+// }
+// const numbers = [10, 20, 30, 40, 50];
+// avg(numbers);
+// console.log(a);
