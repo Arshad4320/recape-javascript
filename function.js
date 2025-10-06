f190b94f2d8f15d6efb936e24fb325b838c8c622
Arshad Ullah <mdarshadullah835@gmail.com>
2025-10-06 11:19:19 +0600

    some change

---
+++ function.js
@@ -0,0 +1,16 @@
+// function add(price1, price2) {
+//   return price1 + price2;
+// }
+// const bill = add(10, 20);
+// console.log(bill);
+
+function stringSize(str) {
+  if (str.length % 2 === 0) {
+    console.log("even size str");
+  } else {
+    console.log("odd size str");
+  }
+  console.log(str, str.length);
+}
+stringSize("Bangladesh");
+stringSize("India");
