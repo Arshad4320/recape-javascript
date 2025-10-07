c71b9985f1bbbbfdf58afa66ea5f8c22edca5ac9
Arshad Ullah <mdarshadullah835@gmail.com>
2025-10-07 21:01:09 +0600

    some update

---
+++ spread.js
@@ -0,0 +1,8 @@
+const friends = ["jonkar", "mezbah", "anis", "tonmoy", "mir"];
+// const newFriends = ["Geyas", ...friends];
+// console.log(newFriends);
+// const [a, b] = friends;
+// console.log(a, b);
+// const obj = { name: "arshad", age: 25 };
+// const { name, age } = obj;
+// console.log(name, age);
