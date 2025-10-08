9fd85625b76381bd3fd49c48783a03cc62f81d89
Arshad Ullah <mdarshadullah835@gmail.com>
2025-10-09 00:56:15 +0600

    some practice js fundamentals

--- scop.js
+++ scop.js
@@ -1,9 +1,34 @@
-function add(num1, num2) {
-  if (true) {
-  }
-  var num = 10;
-  num = 25;
-  console.log(num);
-  return num1 + num2;
+// function add(num1, num2) {
+//   if (true) {
+//   }
+//   var num = 10;
+//   num = 25;
+//   console.log(num);
+//   return num1 + num2;
+// }
+// console.log(add(10, 20));
+
+//closure function practice;
+
+// function sum() {
+//   let counter = 0;
+//   return function () {
+//     counter++;
+//     console.log(counter);
+//   };
+// }
+// const result = sum();
+// result();
+// result();
+// result();
+
+//callback function ;
+function callback(fn, name) {
+  fn();
+  console.log(name);
 }
-console.log(add(10, 20));
+
+function fn() {
+  console.log("say hello");
+}
+callback(fn, "arshad");
